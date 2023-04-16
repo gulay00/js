342a3fd547a96a1c2f45356210c3138eaa8ed20e
gulay00 <130738219+gulay00@users.noreply.github.com>
2023-04-16 20:24:08 +0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,103 @@
+// 10 dan 1e qeder butun ededler console cixartmaq
+// let n = 10;
+// for(n; n>0; n--){
+//     console.log(n)
+// }
+
+
+// 0-100 intervalinda 9a tam bolunen ededler
+// let n=0
+// for(n;n<100;n++){
+//     if(n%9==0){
+//         console.log(n)
+//     }
+// }
+
+
+// 0-100 arasi intervalinda yerlesen cut ededlerin sayin tapib, console cixardmaq
+// let n=100
+// for(n;n>0;n--){
+//    if(n%2==0){console.log(n)}
+// }
+
+
+
+//ededin sade ve ya murekkeb olmasi
+// let n=17;
+// let i=1;
+
+// let sadeMurekkeb = 0
+
+// for(i;i<=n;i++){
+//     if(n%i==0){
+//         sadeMurekkeb++
+//     }
+
+// }
+// if(sadeMurekkeb>2){
+//     console.log('eded murekkebdir')
+// }else{
+//     console.log('eded sadedir')   
+// }
+
+
+
+// //interval kvadrati
+// let a=10;
+// let b=15;
+//  while(a<=b){
+//     console.log(a**2)
+//     a++
+
+//  }
+
+
+//intervalin cemi
+// let a=3;
+// let b=6;
+// let cem=0;
+// while(a<=b){
+//     cem+=a
+//   a++
+// }
+// console.log(cem)
+
+
+//en boyuk boleni
+// let n=28;
+// let bolen=1;
+// let cavab = 0;
+
+// for(bolen;bolen<n;bolen++){
+//     if(n%bolen==0){
+//         cavab=bolen
+//     }
+// }
+// console.log(cavab)
+
+
+// let x = 1
+// let string = 'x'
+
+// for(x; x<=5;x++){
+//     console.log(string)
+//     string+='x'
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
