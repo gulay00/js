e1c22ca843dca8d7ac7aaf59f1d3d7078430a8a4
gulay00 <130738219+gulay00@users.noreply.github.com>
2023-04-16 20:25:07 +0400

    Add files via upload

---
+++ ilk (1).js
@@ -0,0 +1,70 @@
+// ededin kvadrat ,kubu
+// const a = 5;
+// console.log(a**2)
+// console.log(a**3)
+
+// zaman , yol
+// const v = 60
+// const s = 180
+// const equilazer = s / v * 60 
+
+// console.log(equilazer)
+
+
+// 3ededin en boyuk olan
+// const a = 5
+// const b = 3
+// const c = 2
+
+// if(a>b && a>c){
+//     console.log(a)
+// }else if(b>a && b>c){
+//     console.log(b)
+// }else{
+//     console.log(c)
+// }
+
+// ededin tek veya cut olmagi
+// const a =17
+// if(a % 2 == 1){
+//     console.log('tek')
+// }else{
+//     console.log('cut')
+// }
+
+// heftenin gunleri
+// const n = 5
+// switch(n){
+//     case 1 :
+//         console.log('bazar ertesi');
+//         break;
+//     case 2 :
+//         console.log('cersenbe axsami');
+//         break;
+//     case 3 :
+//         console.log('cersenbe');
+//         break;
+//     case 4 :
+//         console.log('cume axsami');
+//         break;
+//     case 5 :
+//         console.log('cume');
+//         break;
+//     case 6 :
+//         console.log('senbe');
+//         break;
+//     case 7 :
+//         console.log('bazar');
+//         break;
+//     default:
+//         console.log('x');
+// }
+
+// const n = 78;
+// if(n>=0 && n<=10){
+//     console.log('birReqemlidir')
+// }else if(n>=10 && n<=100){
+//     console.log("ikiReqemlidir")
+// }else if(n>=100 && n<1000){
+//     console.log("ucReqemlidir")
+// }else{console.log("ededIntervaldaDeyil")}
